6f7ba9f6e6ac74aa1515b54886f48afe9296a620
lizongti <lzongti@gmail.com>
2021-09-10 12:09:03 +0800

    delete

--- init/superset.sh
+++
@@ -1,5 +0,0 @@
-docker exec -it superset superset fab create-admin --username admin --firstname Superset --lastname Admin --email admin@superset.com --password admin
-docker exec -it superset superset db upgrade
-docker exec -it superset superset init 
-
-docker swarm update --task-history-limit 2
